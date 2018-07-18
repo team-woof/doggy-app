6d4c6b48c0a06928081d0a9cd594c67d07b116a0
Ethan Ng <ethanng329@gmail.com>
2018-07-18 12:02:40 +0100

    first test passed

--- src/server.test.js
+++ src/server.test.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-app.use(bodyParser.json());
-app.use('/static', express.static(__dirname + '/static'));
-
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
 function dogs() {
   const dogsList = {
     '1': {
@@ -109,22 +98,16 @@ function dogs() {
         return [...acc, result];
       }, []);
 
-      // const removeDuplicates = arrX => {
-      //   return arrX.filter((elem, index, arr) => {
-      //     return arr.indexOf(elem) === index;
-      //   });
-      // };
-
-      const resultBreeds = removeDuplicates(breeds);
-
-      return resultBreeds;
+      return breeds;
     },
+
     getLocations() {
-      const location = Object.keys(dogsList).reduce((acc, item, index) => {
-        const result = dogsList[item].location;
-        return [...acc, result];
-      }, []);
-      return location;
+      const result = Object.keys(dogsList).reduce((acc, item, index) => {
+        const currentLocation = dogsList[item].location;
+        acc[currentLocation] = index;
+        return acc;
+      }, {});
+      return result;
     }
   };
 }
@@ -137,51 +120,8 @@ const {
   filterLocation
 } = dogs();
 
-app.get('/api/dogs', function(req, res) {
-  if (getDogs()) {
-    res.json(getDogs());
-  } else {
-    res.status(404).json({ error: 'No dogs found' });
-  }
-});
-
-//http://127.0.0.1:8080/api/query?breed=Labrador
-app.get('/api/query', function(req, res) {
-  if (req.query.breed) {
-    res.json(filterBreed(req.query.breed));
-  } else {
-    res.status(404).json({ error: 'No breed found' });
-  }
-});
-
-//resume this tomorrow
-app.get('/api/breedlocation', function(req, res) {
-  const breed = req.query.breed;
-  const location = req.query.location;
-  const resultBreed = filterBreed(breed);
-  const resultLocation = filterLocation(location);
-  const result = Object.assign({}, ...resultBreed, ...resultLocation);
-
-  res.json({ result });
-
-  // res.json(req.query);
-});
-
-app.get('/api/locations', function(req, res) {
-  if (getLocations()) {
-    res.json(getLocations());
-  } else {
-    res.status(404).json({ error: 'No breed found' });
-  }
-});
-
-app.get('/api/getresultlist', function(req, res) {
-  res.json({
-    location: getLocations(),
-    breeds: getBreeds()
+describe('firsttest', function() {
+  test('location duplicate', () => {
+    expect(getLocations()).toEqual({ Croydon: 1, Liverpool: 2, Shoreditch: 3 });
   });
-});
-
-app.listen(8080, function() {
-  console.log('Listening on port 8080');
 });

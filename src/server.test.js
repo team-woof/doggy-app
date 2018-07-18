2d8842f10edb57dd7abf5d94aa69a87f5108b6f1
Ethan Ng <ethanng329@gmail.com>
2018-07-18 12:29:33 +0100

    getDogs test added

--- src/server.test.js
+++ src/server.test.js
@@ -192,4 +192,80 @@ describe('firsttest', function() {
       }
     ]);
   });
+  test('getDogs', function() {
+    expect(getDogs()).toEqual({
+      '1': {
+        additionalInfo: 'Alergie to cat food',
+        age: '4',
+        breed: 'Labrador',
+        contact: {
+          ownerEmail: 'tom@gmail.com',
+          ownerName: 'Tom Doe',
+          ownerNumber: '07765432100'
+        },
+        description: 'very hairy',
+        hobbies: ['fetching', 'swimming', 'jumping'],
+        id: '1',
+        images: [],
+        location: 'Shoreditch',
+        name: 'Max',
+        personality: 'Nice and friendly',
+        sex: 'male'
+      },
+      '2': {
+        additionalInfo: 'Alergie to cat food',
+        age: '2',
+        breed: 'Terrier',
+        contact: {
+          ownerEmail: 'john@gmail.com',
+          ownerName: 'John Doe',
+          ownerNumber: '07765432100'
+        },
+        description: 'barks a lot',
+        hobbies: ['sleeping', 'swimming', 'jumping'],
+        id: '2',
+        images: [],
+        location: 'Croydon',
+        name: 'Bella',
+        personality: 'Very aggresive',
+        sex: 'female'
+      },
+      '3': {
+        additionalInfo: 'Alergie to cat food',
+        age: '4',
+        breed: 'Beagel',
+        contact: {
+          ownerEmail: 'julian@gmail.com',
+          ownerName: 'Julian Doe',
+          ownerNumber: '07765432100'
+        },
+        description: 'very hairy',
+        hobbies: ['fetching', 'swimming', 'jumping'],
+        id: '3',
+        images: [],
+        location: 'Liverpool',
+        name: 'Buddy',
+        personality: 'Nice and friendly',
+        sex: 'male'
+      },
+      '4': {
+        additionalInfo: 'Alergie to cat food',
+        age: '4',
+        breed: 'Labrador',
+        contact: {
+          ownerEmail: 'tom@gmail.com',
+          ownerName: 'Tom Doe',
+          ownerNumber: '07765432100'
+        },
+        description: 'very hairy',
+        hobbies: ['fetching', 'swimming', 'jumping'],
+        id: '4',
+        images: [],
+        location: 'Shoreditch',
+        name: 'Trump',
+        personality: 'Nice and friendly',
+        sex: 'male'
+      }
+    });
+  });
 });

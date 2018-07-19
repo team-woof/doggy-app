52a82ae60401d72eedad1044cb4f62303adf546f
Julius <juliusvaiciunas@gmail.com>
2018-07-19 16:37:09 +0100

    Fix - Added Header component

--- src/Routes/AboutUsRoute.js
+++ src/Routes/AboutUsRoute.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeContainer from "../Containers/HomeContainer";
 import AboutUsContainer from "../Containers/AboutUsContainer";
+import Header from "../components/Header";
 
 class AccountsRoute extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <AboutUsContainer />
       </div>
     );
--- src/Routes/AccountsRoute.js
+++ src/Routes/AccountsRoute.js
@@ -1,11 +1,13 @@
 import React from "react";
 import HomeContainer from "../Containers/HomeContainer";
 import AccountsContainer from "../Containers/AccountsContainer";
+import Header from "../components/Header";
 
 class AccountsRoute extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <AccountsContainer />
       </div>
     );

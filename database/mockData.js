b44245193e9763cef193791862b4bc65584288c0
Julius <juliusvaiciunas@gmail.com>
2018-07-19 11:20:13 +0100

    added image

--- database/mockData.js
+++ database/mockData.js
@@ -1,75 +1,77 @@
 const dogsList = {
-  '1': {
-    id: '1',
-    name: 'Max',
-    age: '4',
-    sex: 'male',
-    breed: 'Labrador',
-    hobbies: ['fetching', 'swimming', 'jumping'],
-    description: 'very hairy',
-    images: [],
-    personality: 'Nice and friendly',
-    location: 'Shoreditch',
+  "1": {
+    id: "1",
+    name: "Max",
+    age: "4",
+    sex: "male",
+    breed: "Labrador",
+    hobbies: ["fetching", "swimming", "jumping"],
+    description: "very hairy",
+    images: [
+      "https://www.usmagazine.com/wp-content/uploads/2018/05/dog-ducklings-01.jpg"
+    ],
+    personality: "Nice and friendly, loves ducks",
+    location: "Shoreditch",
     contact: {
-      ownerName: 'Tom Doe',
-      ownerNumber: '07765432100',
-      ownerEmail: 'tom@gmail.com'
+      ownerName: "Tom Doe",
+      ownerNumber: "07765432100",
+      ownerEmail: "tom@gmail.com"
     },
-    additionalInfo: 'Alergie to cat food'
+    additionalInfo: "Alergie to cat food"
   },
-  '2': {
-    id: '2',
-    name: 'Bella',
-    age: '2',
-    sex: 'female',
-    breed: 'Terrier',
-    hobbies: ['sleeping', 'swimming', 'jumping'],
-    description: 'barks a lot',
+  "2": {
+    id: "2",
+    name: "Bella",
+    age: "2",
+    sex: "female",
+    breed: "Terrier",
+    hobbies: ["sleeping", "swimming", "jumping"],
+    description: "barks a lot",
     images: [],
-    personality: 'Very aggresive',
-    location: 'Croydon',
+    personality: "Very aggresive",
+    location: "Croydon",
     contact: {
-      ownerName: 'John Doe',
-      ownerNumber: '07765432100',
-      ownerEmail: 'john@gmail.com'
+      ownerName: "John Doe",
+      ownerNumber: "07765432100",
+      ownerEmail: "john@gmail.com"
     },
-    additionalInfo: 'Alergie to cat food'
+    additionalInfo: "Alergie to cat food"
   },
-  '3': {
-    id: '3',
-    name: 'Buddy',
-    age: '4',
-    sex: 'male',
-    breed: 'Beagel',
-    hobbies: ['fetching', 'swimming', 'jumping'],
-    description: 'very hairy',
+  "3": {
+    id: "3",
+    name: "Buddy",
+    age: "4",
+    sex: "male",
+    breed: "Beagel",
+    hobbies: ["fetching", "swimming", "jumping"],
+    description: "very hairy",
     images: [],
-    personality: 'Nice and friendly',
-    location: 'Liverpool',
+    personality: "Nice and friendly",
+    location: "Liverpool",
     contact: {
-      ownerName: 'Julian Doe',
-      ownerNumber: '07765432100',
-      ownerEmail: 'julian@gmail.com'
+      ownerName: "Julian Doe",
+      ownerNumber: "07765432100",
+      ownerEmail: "julian@gmail.com"
     },
-    additionalInfo: 'Alergie to cat food'
+    additionalInfo: "Alergie to cat food"
   },
-  '4': {
-    id: '4',
-    name: 'Trump',
-    age: '4',
-    sex: 'male',
-    breed: 'Labrador',
-    hobbies: ['fetching', 'swimming', 'jumping'],
-    description: 'very hairy',
+  "4": {
+    id: "4",
+    name: "Trump",
+    age: "4",
+    sex: "male",
+    breed: "Labrador",
+    hobbies: ["fetching", "swimming", "jumping"],
+    description: "very hairy",
     images: [],
-    personality: 'Nice and friendly',
-    location: 'Shoreditch',
+    personality: "Nice and friendly",
+    location: "Shoreditch",
     contact: {
-      ownerName: 'Tom Doe',
-      ownerNumber: '07765432100',
-      ownerEmail: 'tom@gmail.com'
+      ownerName: "Tom Doe",
+      ownerNumber: "07765432100",
+      ownerEmail: "tom@gmail.com"
     },
-    additionalInfo: 'Alergie to cat food'
+    additionalInfo: "Alergie to cat food"
   }
 };
 

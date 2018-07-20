e925bd8df0b33251184e8d3e3b27ef93e179b695
Ralph <ralph@rgalan.es>
2018-07-20 12:27:48 +0100

    Added image to about us

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,40 +8,15 @@ export class AboutUs extends Component {
       <div className="aboutUs">
         <div className="form">
           <h1 className="main__header">About us</h1>
+          <img className="about-us__img" src="../../static/img/about.jpg" alt="about us" />
           <p>
             Doggo ipsum doing me a frighten fluffer woofer very good spot vvv,
             noodle horse I am bekom fat lotsa pats borkf, heckin clouds I am
             bekom fat. big ol pupper wow such tempt. Doggorino borking doggo
             corgo most angery pupper I have ever seen heckin good boys and
             girls, woofer wow very biscit. Clouds tungg boof sub woofer long
-            doggo blep, fluffer wow very biscit borking doggo. Thicc very good
-            spot waggy wags sub woofer pupperino, heck thicc doing me a
-            frighten, you are doing me the shock shoob he made many woofs. Long
-            bois big ol blep, yapper. Heckin angery woofer wow such tempt snoot
-            extremely cuuuuuute dat tungg tho vvv, ur givin me a spook fluffer
-            doge. Most angery pupper I have ever seen adorable doggo puggorino
-            ruff I am bekom fat heckin good boys and girls, doggo bork doing me
-            a frighten. Very good spot ruff you are doing me a frighten wow very
-            biscit, heckin clouds big ol pupper, puggorino the neighborhood
-            pupper. Vvv adorable doggo puggo big ol pupper pats, aqua doggo
-            fluffer doggo much ruin diet ur givin me a spook, many pats much
-            ruin diet pats. Smol he made many woofs shoober boofers borkf long
-            water shoob, yapper woofer mlem. Shoob waggy wags wow such tempt
-            corgo long doggo smol borking doggo with a long snoot for pats mlem
-            many pats pupper clouds, ur givin me a spook puggorino shibe corgo
-            shoober shoob boof. Many pats lotsa pats shoob ur givin me a spook
-            what a nice floof, doggo you are doing me a frighten. Such treat
-            heckin ruff big ol super chub length boy, super chub wow very biscit
-            shooberino. Very hand that feed shibe h*ck you are doing me a
-            frighten you are doin me a concern ruff, mlem maximum borkdrive what
-            a nice floof, thicc doggorino shoob. Waggy wags length boy puggo
-            doggo, noodle horse. Vvv you are doin me a concern smol noodle horse
-            very hand that feed shibe many pats snoot puggo, pats stop it fren
-            woofer fluffer length boy. Vvv blep stop it fren clouds borkf,
-            boofers doing me a frighten. Maximum borkdrive what a nice floof
-            shooberino heckin, floofs lotsa pats. Thicc pats snoot, extremely
-            cuuuuuute. Adorable doggo thicc woofer shoober, borkf very taste wow
-            thicc, extremely cuuuuuute pats.
+            doggo blep, fluffer wow very biscit borking doggo.
+            doge.
           </p>
         </div>
       </div>

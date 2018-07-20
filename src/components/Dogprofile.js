93b4f13e494691060596ca214ae407726c9cfccd
Ralph <ralph@rgalan.es>
2018-07-20 10:35:41 +0100

    Styling update

--- src/components/Dogprofile.js
+++ src/components/Dogprofile.js
@@ -20,38 +20,39 @@ export class Dogprofile extends React.Component {
           <div className="Dogprofile__information">
             {this.props.searchResults.length > 0
               ? this.props.searchResults.map(result => {
-                  if (result.id === this.props.profileId) {
-                    return (
-                      <div
-                        className="Dogprofile__img__container"
-                        key={result.name}
-                      >
-                        <img className="Dogprofile__img" src={result.images} />
-                        <div className="Dogprofile__details">
-                          <p>name: {result.name}</p>
-                          <p>age: {result.age}</p>
-                          <p>sex: {result.gender}</p>
-                          <p>breed: {result.breed}</p>
-                          <div>
-                            <h3>Hobbies</h3>
-                            <ul>
-                              {result.hobbies.map(hobby => {
-                                return <li>{hobby}</li>;
-                              })}
-                            </ul>
-                          </div>
-                          <p>description: {result.description}</p>
-                          <p>personality: {result.personality}</p>
-                          <p>location: {result.location},</p>
-                          <p> contact: {result.contact.ownerName}</p>
-                          <p> contact: {result.contact.ownerNumber}</p>
-                          <p> contact: {result.contact.ownerEmail}</p>
-                          <p>additionalInfo: {result.additionalInfo}</p>
+                if (result.id === this.props.profileId) {
+                  return (
+                    <div
+                      className="Dogprofile__img__container"
+                      key={result.name}
+                    >
+                      <img className="Dogprofile__img" src={result.images} />
+                      <div className="Dogprofile__details">
+                        <div><strong>Name:</strong> {result.name}</div>
+                        <div><strong>Age:</strong> {result.age}</div>
+                        <div><strong>Gender:</strong> {result.gender}</div>
+                        <div><strong>Breed:</strong> {result.breed}</div>
+                        <div>
+                          <h3>Hobbies</h3>
+                          <ul>
+                            {result.hobbies.map(hobby => {
+                              return <li>{hobby}</li>;
+                            })}
+                          </ul>
                         </div>
+                        <div><strong>Description:</strong> {result.description}</div>
+                        <div><strong>Personality:</strong> {result.personality}</div>
+                        <div><strong>Location:</strong> {result.location},</div>
+                        <div><strong>Contact:</strong> {result.contact.ownerName}</div>
+                        <div>{result.contact.ownerNumber}</div>
+                        <div>{result.contact.ownerEmail}</div>
+                        <div><strong>Additional info:</strong> {result.additionalInfo}</div>
+                        <a className="dogprofile__email-button" href={"mailto:" + result.contact.ownerEmail + "?subject=Contact from ChasingTheTail"}>Email owner</a>
                       </div>
-                    );
-                  }
-                })
+                    </div>
+                  );
+                }
+              })
               : 'No profile selected'}
           </div>
         </div>

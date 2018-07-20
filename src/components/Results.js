2d2e8fcbd9cd7e1301a066ab207e40887292a0d5
Ralph <ralph@rgalan.es>
2018-07-20 13:32:53 +0100

    Added transparency to login form background

--- src/components/Results.js
+++ src/components/Results.js
@@ -34,39 +34,39 @@ class Results extends React.Component {
       <div className="results-container">
         {isArray(this.props.searchResults)
           ? this.props.searchResults.map(result => {
-              return (
-                <div key={result.id} className="result-card">
-                  {this.renderRedirect()}
-                  <div className="result-card__img-box">
-                    <img
-                      className="result-card__img"
-                      src={result.images}
-                      alt={result.name}
-                    />
-                  </div>
-                  <div className="result-card__details">
-                    <div className="result-card__name">
-                      <strong>Name: </strong>
-                      {result.name}
-                    </div>
-                    <div className="result-card__location">
-                      <strong>Location: </strong>
-                      {result.location}
-                    </div>
-                    <div className="result-card__gender">
-                      <strong>Gender: </strong>
-                      {result.gender}
-                    </div>
-                  </div>
-                  <button
-                    onClick={() => this.onClickHandler(result.id)}
-                    className="result-card__profile-button"
-                  >
-                    Full Profile
-                  </button>
+            return (
+              <div key={result.id} className="result-card">
+                {this.renderRedirect()}
+                <div className="result-card__img-box">
+                  <img
+                    className="result-card__img"
+                    src={result.images}
+                    alt={result.name}
+                  />
                 </div>
-              );
-            })
+                <div className="result-card__details">
+                  <div className="result-card__name">
+                    <strong>Name: </strong>
+                    {result.name}
+                  </div>
+                  <div className="result-card__location">
+                    <strong>Location: </strong>
+                    {result.location}
+                  </div>
+                  <div className="result-card__gender">
+                    <strong>Gender: </strong>
+                    {result.gender}
+                  </div>
+                </div>
+                <button
+                  onClick={() => this.onClickHandler(result.id)}
+                  className="result-card__profile-button"
+                >
+                  Full Profile
+                  </button>
+              </div>
+            );
+          })
           : ''}
       </div>
     );

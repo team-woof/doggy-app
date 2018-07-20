e0236fbd9f93bc4b932b33b3d6583928826922de
Julius <juliusvaiciunas@gmail.com>
2018-07-20 14:10:02 +0100

    added heroku Procfile, added node dependecy, changed default port listener

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 const {
@@ -9,53 +9,54 @@ const {
   getBreeds,
   filterByQuery,
   filterLocation
-} = require('./database/database');
+} = require("./database/database");
 
 app.use(bodyParser.json());
-app.use('/static', express.static(__dirname + '/static'));
+app.use("/static", express.static(__dirname + "/static"));
 
-app.get('/api/dogs', function(req, res) {
+app.get("/api/dogs", function(req, res) {
   if (getDogs()) {
     res.json(getDogs());
   } else {
-    res.status(404).json({ error: 'No dogs found' });
+    res.status(404).json({ error: "No dogs found" });
   }
 });
 
 //http://127.0.0.1:8080/api/query?breed=Labrador
-app.get('/api/query', function(req, res) {
+app.get("/api/query", function(req, res) {
   if (req.query.breed) {
     res.json(filterBreed(req.query.breed));
   } else {
-    res.status(404).json({ error: 'No breed found' });
+    res.status(404).json({ error: "No breed found" });
   }
 });
 
 //r http://127.0.0.1:8080/api/breedlocation?breed=Labrador&location=london
-app.get('/api/breedlocation', function(req, res) {
+app.get("/api/breedlocation", function(req, res) {
   const { breed, location } = req.query;
   res.json(filterByQuery(breed, location));
 });
 
-app.get('/api/locations', function(req, res) {
+app.get("/api/locations", function(req, res) {
   if (getLocations()) {
     res.json(getLocations());
   } else {
-    res.status(404).json({ error: 'No breed found' });
+    res.status(404).json({ error: "No breed found" });
   }
 });
 
-app.get('/api/getresultlist', function(req, res) {
+app.get("/api/getresultlist", function(req, res) {
   res.json({
     location: getLocations(),
     breeds: getBreeds()
   });
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080, function() {
-  console.log('Listening on port 8080');
+const port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log(`Listening on port number ${port}`);
 });

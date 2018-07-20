2b2af069f5bc3e2d689c2c1634e3a3f9324fcad8
James McAllister <jrfmcallister@gmail.com>
2018-07-20 15:07:04 +0100

    auth added. need to show errors and logout etc

--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -3,33 +3,86 @@ import React, { Component } from "react";
 import "../../static/styles/components/account.scss";
 
 export class Accounts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false,
+      username: ""
+    };
+  }
+  authWithServer({ username, password }) {
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        accept: "application/json"
+      },
+      body: JSON.stringify({ username, password })
+    })
+      .then(resp => {
+        if (resp.status == 200) {
+          return resp.json();
+        } else {
+          return resp.text();
+        }
+      })
+      .then(data => {
+        if (data.access_token) {
+          console.log(`Access Token: ${data.access_token}`);
+          this.setState({ loggedIn: true, username: username });
+        }
+      })
+      .then(data => console.log(data));
+  }
+
+  handelSumbit(event) {
+    event.preventDefault();
+
+    const username = event.target[0].value;
+    const password = event.target[1].value;
+    console.log("wof", { username, password });
+    this.authWithServer({ username, password });
+  }
   render() {
     return (
       <div className="Accounts">
         <div className="video-container">
-          <div className="video__overlay"></div>
+          <div className="video__overlay" />
           <video className="video" autoPlay muted loop>
             <source src="../../static/videos/dog02.mp4" type="video/mp4" />
           </video>
         </div>
         <div className="login-page">
           <div className="form">
-            <form className="register-form">
+            {/* <form className="register-form">
               <input type="text" placeholder="name" />
-              <input type="password" placeholder="password" />
+              <input type="current-password" placeholder="current-password" />
               <input type="text" placeholder="email address" />
               <button>create</button>
               <p className="message">
                 Already registered? <a href="#">Sign In</a>
               </p>
-            </form>
-            <form className="login-form">
+            </form> */}
+            <form
+              className="login-form"
+              onSubmit={event => this.handelSumbit(event)}
+            >
               <input type="text" placeholder="username" />
-              <input type="password" placeholder="password" />
-              <button>login</button>
-              <p className="message">
-                Not registered? <a href="#">Create an account</a>
-              </p>
+              <input
+                type="current-password"
+                ref="currentPassword"
+                placeholder="current-password"
+              />
+              <button type="submit">login</button>
+              {!this.state.loggedIn ? (
+                <p className="message">
+                  Not registered? <a href="#">Create an account</a>
+                </p>
+              ) : (
+                <p className="message">
+                  You are now loged in as {this.state.username}
+                </p>
+              )}
             </form>
           </div>
         </div>
--- src/components/Dogprofile.js
+++ src/components/Dogprofile.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import Header from './Header';
-import '../../static/styles/components/Dogprofile.scss';
+import Header from "./Header";
+import "../../static/styles/components/dogprofile.scss";
 
 // import styles
 
@@ -20,40 +20,68 @@ export class Dogprofile extends React.Component {
           <div className="Dogprofile__information">
             {this.props.searchResults.length > 0
               ? this.props.searchResults.map(result => {
-                if (result.id === this.props.profileId) {
-                  return (
-                    <div
-                      className="Dogprofile__img__container"
-                      key={result.name}
-                    >
-                      <img className="Dogprofile__img" src={result.images} />
-                      <div className="Dogprofile__details">
-                        <div><strong>Name:</strong> {result.name}</div>
-                        <div><strong>Age:</strong> {result.age}</div>
-                        <div><strong>Gender:</strong> {result.gender}</div>
-                        <div><strong>Breed:</strong> {result.breed}</div>
-                        <div>
-                          <h3>Hobbies</h3>
-                          <ul>
-                            {result.hobbies.map(hobby => {
-                              return <li>{hobby}</li>;
-                            })}
-                          </ul>
+                  if (result.id === this.props.profileId) {
+                    return (
+                      <div
+                        className="Dogprofile__img__container"
+                        key={result.name}
+                      >
+                        <img className="Dogprofile__img" src={result.images} />
+                        <div className="Dogprofile__details">
+                          <div>
+                            <strong>Name:</strong> {result.name}
+                          </div>
+                          <div>
+                            <strong>Age:</strong> {result.age}
+                          </div>
+                          <div>
+                            <strong>Gender:</strong> {result.gender}
+                          </div>
+                          <div>
+                            <strong>Breed:</strong> {result.breed}
+                          </div>
+                          <div>
+                            <h3>Hobbies</h3>
+                            <ul>
+                              {result.hobbies.map(hobby => {
+                                return <li>{hobby}</li>;
+                              })}
+                            </ul>
+                          </div>
+                          <div>
+                            <strong>Description:</strong> {result.description}
+                          </div>
+                          <div>
+                            <strong>Personality:</strong> {result.personality}
+                          </div>
+                          <div>
+                            <strong>Location:</strong> {result.location},
+                          </div>
+                          <div>
+                            <strong>Contact:</strong> {result.contact.ownerName}
+                          </div>
+                          <div>{result.contact.ownerNumber}</div>
+                          <div>{result.contact.ownerEmail}</div>
+                          <div>
+                            <strong>Additional info:</strong>{" "}
+                            {result.additionalInfo}
+                          </div>
+                          <a
+                            className="dogprofile__email-button"
+                            href={
+                              "mailto:" +
+                              result.contact.ownerEmail +
+                              "?subject=Contact from ChasingTheTail"
+                            }
+                          >
+                            Email owner
+                          </a>
                         </div>
-                        <div><strong>Description:</strong> {result.description}</div>
-                        <div><strong>Personality:</strong> {result.personality}</div>
-                        <div><strong>Location:</strong> {result.location},</div>
-                        <div><strong>Contact:</strong> {result.contact.ownerName}</div>
-                        <div>{result.contact.ownerNumber}</div>
-                        <div>{result.contact.ownerEmail}</div>
-                        <div><strong>Additional info:</strong> {result.additionalInfo}</div>
-                        <a className="dogprofile__email-button" href={"mailto:" + result.contact.ownerEmail + "?subject=Contact from ChasingTheTail"}>Email owner</a>
                       </div>
-                    </div>
-                  );
-                }
-              })
-              : 'No profile selected'}
+                    );
+                  }
+                })
+              : "No profile selected"}
           </div>
         </div>
       </div>
